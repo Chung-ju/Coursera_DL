0e4353027f450358ab7b7b848c3bfabd1d4eb713
Chongru Huang <chongruhuang@outlook.com>
2022-05-14 16:42:41 +0800

    Update dnn_app_utils_v3.py

--- 01_Neural_Networks_and_Deep_Learning/Week4/W4A2-Deep Neural Network Application_ Image Classification/dnn_app_utils_v3.py
+++ 01_Neural_Networks_and_Deep_Learning/Week4/W4A2-Deep Neural Network Application_ Image Classification/dnn_app_utils_v3.py
@@ -85,11 +85,11 @@ def sigmoid_backward(dA, cache):
 
 
 def load_data():
-    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+    train_dataset = h5py.File('/content/drive/MyDrive/Coursera_DL/01_Neural_Networks_and_Deep_Learning/Week4/W4A2-Deep Neural Network Application_ Image Classification/datasets/train_catvnoncat.h5', "r")
     train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
     train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
 
-    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
+    test_dataset = h5py.File('/content/drive/MyDrive/Coursera_DL/01_Neural_Networks_and_Deep_Learning/Week4/W4A2-Deep Neural Network Application_ Image Classification/datasets/test_catvnoncat.h5', "r")
     test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
     test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
 
